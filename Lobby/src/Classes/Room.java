f17b53cdee06fb983b873aa3f6c8de32cb4c6446
Tomas Araujo <tomas.araujo@correo.unimet.edu.ve>
2024-03-12 07:55:23 -0400

    Creación clases
    Se hicieron los constructores y getters y setters de las clases reservación y habitación.

--- Lobby/src/Classes/Room.java
+++ Lobby/src/Classes/Room.java
@@ -9,5 +9,60 @@ package Classes;
  * @author tomasaraujo
  */
 public class Room {
+    private int roomNum;
+    private String roomType;
+    private int floor;
+
+    public Room(int roomNum, String roomType, int floor) {
+        this.roomNum = roomNum;
+        this.roomType = roomType;
+        this.floor = floor;
+    }
+
+    /**
+     * @return the roomNum
+     */
+    public int getRoomNum() {
+        return roomNum;
+    }
+
+    /**
+     * @param roomNum the roomNum to set
+     */
+    public void setRoomNum(int roomNum) {
+        this.roomNum = roomNum;
+    }
+
+    /**
+     * @return the roomType
+     */
+    public String getRoomType() {
+        return roomType;
+    }
+
+    /**
+     * @param roomType the roomType to set
+     */
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    /**
+     * @return the floor
+     */
+    public int getFloor() {
+        return floor;
+    }
+
+    /**
+     * @param floor the floor to set
+     */
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
     
+    @Override
+    public String toString() {
+        return "Numero de habitacion: " + roomNum + "\nTipo de habitación: " + roomType + "\nPiso: " + floor;
+    }
 }
