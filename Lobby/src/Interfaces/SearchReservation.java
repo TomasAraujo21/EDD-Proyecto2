d6d804a7105ae0e3e30927d25a97cd3b434c0fe2
Christian Rodríguez <ch.rodriguez@correo.unimet.edu.ve>
2024-03-12 18:56:46 -0400

    Interfaces actualizacion 1
    Se añadio la funcionalidad de los botones para navegar a traves de las interfaces y la imagen de fondo.

--- Lobby/src/Interfaces/SearchReservation.java
+++ Lobby/src/Interfaces/SearchReservation.java
@@ -6,15 +6,23 @@ package Interfaces;
 
 /**
  *
- * @author tomasaraujo
+ * @author Christian
  */
 public class SearchReservation extends javax.swing.JFrame {
 
+    
     /**
      * Creates new form FindReservation
      */
-    public SearchReservation() {
-        initComponents();
+    
+        public static Menu v1;
+
+        public SearchReservation(Menu v1) {
+            initComponents();
+            this.v1 = v1;
+            v1.setVisible(false);
+            this.setLocationRelativeTo(null);
+            this.setResizable(false); 
     }
 
     /**
@@ -26,11 +34,81 @@ public class SearchReservation extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        back = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        search = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        searchText = new javax.swing.JTextField();
+        background = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        back.setBackground(new java.awt.Color(0, 0, 0));
+        back.setForeground(new java.awt.Color(255, 255, 255));
+        back.setText("Volver");
+        back.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backActionPerformed(evt);
+            }
+        });
+        jPanel1.add(back, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 310, 90, 40));
+
+        jLabel3.setFont(new java.awt.Font("Britannic Bold", 0, 30)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Buscar reservación");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 270, 30));
+
+        search.setBackground(new java.awt.Color(0, 0, 0));
+        search.setForeground(new java.awt.Color(255, 255, 255));
+        search.setText("Buscar");
+        search.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchActionPerformed(evt);
+            }
+        });
+        jPanel1.add(search, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 90, 70, 30));
+
+        jLabel1.setFont(new java.awt.Font("Britannic Bold", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Introduce los datos de la reservación:");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, -1, -1));
+
+        searchText.setBackground(new java.awt.Color(255, 255, 255));
+        searchText.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        searchText.setForeground(new java.awt.Color(0, 0, 0));
+        searchText.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchTextActionPerformed(evt);
+            }
+        });
+        jPanel1.add(searchText, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 90, 310, 30));
+
+        background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/background.jpg"))); // NOI18N
+        jPanel1.add(background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 380));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 470, 380));
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void backActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backActionPerformed
+        this.setVisible(false);
+        Welcome b = new Welcome();
+        Menu window1 = new Menu(b);
+        window1.setVisible(true);
+    }//GEN-LAST:event_backActionPerformed
+
+    private void searchTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchTextActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_searchTextActionPerformed
+
+    private void searchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchActionPerformed
+        // Boton de buscar
+    }//GEN-LAST:event_searchActionPerformed
 
     /**
      * @param args the command line arguments
@@ -63,11 +141,18 @@ public class SearchReservation extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new SearchReservation().setVisible(true);
+                new SearchReservation(v1).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton back;
+    private javax.swing.JLabel background;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton search;
+    private javax.swing.JTextField searchText;
     // End of variables declaration//GEN-END:variables
 }
