ebaf3bccc35fcbb78402054f55bed8abe9b33ddb
Tomas Araujo <tomas.araujo@correo.unimet.edu.ve>
2024-03-12 22:03:11 -0400

    Creación clase Client y modificación clase Reservation

--- Lobby/src/Classes/Reservation.java
+++ Lobby/src/Classes/Reservation.java
@@ -9,96 +9,30 @@ package Classes;
  * @author tomasaraujo
  */
 public class Reservation {
-    private int id;
-    private String name;
-    private String lastname;
-    private String email;
-    private String sex;
+    private Client client;
     private String roomType;
-    private String phoneNumber;
     private String checkIn;
     private String checkOut;
 
-    public Reservation(int id, String name, String lastname, String email, String sex, String roomType, String phoneNumber, String checkIn, String checkOut) {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
-        this.email = email;
-        this.sex = sex;
+    public Reservation(Client client, String roomType, String checkIn, String checkOut) {
+        this.client = client;
         this.roomType = roomType;
-        this.phoneNumber = phoneNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
 
     /**
-     * @return the id
+     * @return the client
      */
-    public int getId() {
-        return id;
+    public Client getClient() {
+        return client;
     }
 
     /**
-     * @param id the id to set
+     * @param client the client to set
      */
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return the lastname
-     */
-    public String getLastname() {
-        return lastname;
-    }
-
-    /**
-     * @param lastname the lastname to set
-     */
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * @return the sex
-     */
-    public String getSex() {
-        return sex;
-    }
-
-    /**
-     * @param sex the sex to set
-     */
-    public void setSex(String sex) {
-        this.sex = sex;
+    public void setClient(Client client) {
+        this.client = client;
     }
 
     /**
@@ -113,20 +47,6 @@ public class Reservation {
      */
     public void setRoomType(String roomType) {
         this.roomType = roomType;
-    }
-
-    /**
-     * @return the phoneNumber
-     */
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    /**
-     * @param phoneNumber the phoneNumber to set
-     */
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     /**
@@ -156,6 +76,9 @@ public class Reservation {
     public void setCheckOut(String checkOut) {
         this.checkOut = checkOut;
     }
-    
-    
+
+    @Override
+    public String toString() {
+        return "Nombre cliente: " + client.getName() + client.getLastname() + "Cedula cliente: " + client.getId() + "LLegada: " + checkIn + "Salida: " + checkOut;
+    }
 }
