415debb18afaa6d3772592b227ea0e3996554e1d
Tomas Araujo <tomas.araujo@correo.unimet.edu.ve>
2024-03-14 08:00:03 -0400

    Update History.java

--- Lobby/src/Classes/History.java
+++ Lobby/src/Classes/History.java
@@ -9,5 +9,75 @@ package Classes;
  * @author tomasaraujo
  */
 public class History {
+    private Client client;
+    private String dateIn;
+    private String dateOut;
+    private String roomNumber;
+
+    public History(Client client, String dateIn, String dateOut, String roomNumber) {
+        this.client = client;
+        this.dateIn = dateIn;
+        this.dateOut = dateOut;
+        this.roomNumber = roomNumber;
+    }
+
+    /**
+     * @return the client
+     */
+    public Client getClient() {
+        return client;
+    }
+
+    /**
+     * @param client the client to set
+     */
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    /**
+     * @return the dateIn
+     */
+    public String getDateIn() {
+        return dateIn;
+    }
+
+    /**
+     * @param dateIn the dateIn to set
+     */
+    public void setDateIn(String dateIn) {
+        this.dateIn = dateIn;
+    }
+
+    /**
+     * @return the dateOut
+     */
+    public String getDateOut() {
+        return dateOut;
+    }
+
+    /**
+     * @param dateOut the dateOut to set
+     */
+    public void setDateOut(String dateOut) {
+        this.dateOut = dateOut;
+    }
+
+    /**
+     * @return the roomNumber
+     */
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    /**
+     * @param roomNumber the roomNumber to set
+     */
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
     
+    public void show() {
+        
+    }
 }
